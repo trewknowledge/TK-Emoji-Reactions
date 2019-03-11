b010bc175c3696ae942f22308c8b054b3e42a201
Fernando Claussen <fernandoclaussen@gmail.com>
2019-03-11 16:55:06 -0400

    MVP

---
+++ assets/js/public.js
@@ -0,0 +1,98 @@
+(function ($) {
+
+	var TKEmojiReaction = {
+		addReaction: function( container, emoji ) {
+			var emojiImage = emojione.shortnameToImage( emoji );
+
+			var clone = container.find( '.reaction.template' ).clone();
+			clone.attr( 'data-emoji', emoji );
+			clone.find('button').prepend( emojiImage );
+			TKEmojiReaction.updateCount( clone );
+			clone.removeClass( 'template' ).addClass('tker_active');
+			clone.css('display', 'inline-block');
+			container.find('.emojionearea').after( clone );
+		},
+		updateCount: function( node, add = true ) {
+			var count = node.find( '.count' ).text();
+			if ( add ) {
+				count++;
+				node.addClass('tker_active');
+			} else {
+				count--;
+				node.removeClass('tker_active');
+			}
+			node.find('.count').text( count );
+			if ( count === 0 ) {
+				node.remove();
+			}
+		},
+		saveReactionToDB: function( container, comment_id, emoji ) {
+			if ( TKER.user_id == 0 ) {
+				$(document).trigger('tker_ask_for_login');
+				return;
+			}
+
+			$.ajax({
+				method: "POST",
+				url: TKER.ajaxurl,
+				data: {
+					action: 'tk_emoji_reaction_save',
+					nonce: TKER.nonce,
+					user_id: TKER.user_id,
+					comment_id: comment_id,
+					emoji: emoji,
+				},
+				success: function(res) {
+					if ( res.success ) {
+						var existingDiv = container.find( '[data-emoji="' + emoji + '"]' );
+						if ( existingDiv.length ) {
+							TKEmojiReaction.updateCount( existingDiv, res.data );
+						} else {
+							TKEmojiReaction.addReaction( container, emoji );
+						}
+					}
+				},
+			});
+		}
+	}
+
+	$( function() {
+
+		$( ".tker-reaction-picker" ).emojioneArea( {
+	    standalone: true,
+	    autocomplete: false,
+	    emojiPlaceholder: "",
+	    pickerPosition: 'right',
+	    filters: {
+	    	recent: false
+	    },
+	    saveEmojisAs: 'unicode',
+	    events: {
+	    	emojibtn_click: function ( btn ) {
+	    		var emojiName = btn.data( 'name' );
+	    		var container = btn.closest( '.comment-reactions' );
+	    		// var existingDiv = container.find( '[data-emoji="' + emojiName + '"]' );
+	    		var comment_id = container.attr('data-comment-id');
+
+    			TKEmojiReaction.saveReactionToDB( container, comment_id, emojiName );
+
+	    		// if ( ! existingDiv.length ) {
+	    			//Save to DB.
+	    			// TKEmojiReaction.addReaction( container, emojiName );
+	    		// } else {
+	    			// TKEmojiReaction.updateReactionCount( existingDiv, true ); // add
+	    			// saveReactionToDB( comment_id, emojiName );
+	    		// }
+	    	}
+	    }
+		});
+
+		$(document).on('click', '.reaction button', function( e ) {
+			e.preventDefault();
+			var container = $(this).closest( '.comment-reactions' );
+			var comment_id = container.attr('data-comment-id');
+			var emojiName = $(this).parent().data( 'emoji' );
+			TKEmojiReaction.saveReactionToDB( container, comment_id, emojiName );
+		});
+	});
+})(jQuery);
---
+++ tk-emoji-reaction.php
@@ -0,0 +1,147 @@
+<?php
+/**
+ * Plugin Name:     Trew Knowledge - Comment Reaction Emojis
+ * Plugin URI:      https://trewknowledge.com
+ * Description:     Enables a series of emojis to be used as reactions to comments.
+ * Author:          Trew Knowledge
+ * Author URI:      https://trewknowledge.com
+ * Text Domain:     tk-emoji-reaction
+ * Domain Path:     /languages
+ * Version:         1.0.0
+ *
+ * @package         Tk_Emoji_Reaction
+ */
+
+require 'vendor/autoload.php';
+
+use Emojione\Emojione;
+use Emojione\Client as EmojioneClient;
+
+define( 'TK_EMOJI_REACTION_VERSION', '1.0.0' );
+
+add_action( 'wp_enqueue_scripts', function() {
+	wp_enqueue_style( 'tk-emoji-reaction-emoji-css', plugin_dir_url( __FILE__ ) . 'assets/css/emojionearea.min.css', array(), TK_EMOJI_REACTION_VERSION );
+	wp_enqueue_script( 'tk-emoji-reaction-emoji-js', plugin_dir_url( __FILE__ ) . 'assets/js/emojionearea.min.js', array(), TK_EMOJI_REACTION_VERSION, true );
+
+	wp_enqueue_style( 'tk-emoji-reaction-public-css', plugin_dir_url( __FILE__ ) . 'assets/css/public.css', array(), TK_EMOJI_REACTION_VERSION );
+	wp_enqueue_script( 'tk-emoji-reaction-public-js', plugin_dir_url( __FILE__ ) . 'assets/js/public.js', array( 'jquery', 'tk-emoji-reaction-emoji-js' ), TK_EMOJI_REACTION_VERSION, true );
+
+	wp_localize_script( 'tk-emoji-reaction-public-js', 'TKER', array(
+		'ajaxurl' => admin_url( 'admin-ajax.php' ),
+		'nonce' => wp_create_nonce( 'tk_save_reaction_nonce' ),
+		'user_id' => get_current_user_id(),
+	) );
+});
+
+
+add_filter( 'wp_list_comments_args', function( $args ) {
+	if ( empty( $args['callback'] ) ) {
+		$args['end-callback'] = 'tker_comment_callback';
+	}
+	return $args;
+});
+
+function tker_comment_callback( $comment, $args, $depth ) {
+	// Load emoji instead of shortname.
+	$emojioneClient = new EmojioneClient();
+	Emojione::setClient( $emojioneClient );
+
+	$reactions      = get_comment_meta( $comment->comment_ID, 'tker_reactions', true );
+	$reactions      = $reactions ?: array();
+	$user_reactions = get_user_meta( get_current_user_id(), "tker_comment_id_{$comment->comment_ID}", true );
+	$user_reactions = $user_reactions ?: array();
+	?>
+	<div class="comment-reactions" data-comment-id="<?php echo esc_attr( $comment->comment_ID ); ?>">
+		<div class="reaction template" style="display: none;">
+			<button type="button">
+				<span class="emoji"></span>
+				<span class="count"></span>
+			</button>
+		</div>
+		<div class="tker-reaction-picker"></div>
+		<?php if ( ! empty( $reactions ) ): ?>
+			<?php $emoji_kses = array(
+				'img' => array(
+					'src' => true,
+					'class' => true,
+					'title' => true,
+				),
+			); ?>
+			<?php foreach ( $reactions as $emoji => $count ): ?>
+				<?php
+					$active = '';
+					if ( in_array( $emoji, $user_reactions, true ) ) {
+						$active = 'tker_active';
+					}
+				?>
+				<div class="reaction <?php echo esc_attr( $active ); ?>" data-emoji="<?php echo esc_attr( $emoji ); ?>">
+					<button type="button">
+						<?php echo wp_kses( Emojione::shortnameToImage( $emoji ), $emoji_kses ) ; ?>
+						<span class="count"><?php echo esc_html( $count ); ?></span>
+					</button>
+				</div>
+			<?php endforeach ?>
+		<?php endif ?>
+	</div>
+	<?php
+}
+
+add_action( 'wp_ajax_tk_emoji_reaction_save', function() {
+	error_log(print_r($_POST, true));
+	if ( empty( $_POST['nonce'] ) || ! wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST['nonce'] ) ), 'tk_save_reaction_nonce' ) ) {
+		wp_send_json_error( 'Nonce failed' );
+	}
+	if ( empty( $_POST['user_id'] ) ) {
+		wp_send_json_error( 'User ID is required.' );
+	}
+	if ( empty( $_POST['comment_id'] ) ) {
+		wp_send_json_error( 'Comment ID is required.' );
+	}
+	if ( empty( $_POST['emoji'] ) ) {
+		wp_send_json_error( 'A reaction is required.' );
+	}
+
+	$user_id           = absint( $_POST['user_id'] );
+	$comment_id        = absint( $_POST['comment_id'] );
+	$emoji             = sanitize_text_field( wp_unslash( $_POST['emoji'] ) );
+	$user_reacted      = false;
+	$user_reactions    = get_user_meta( $user_id, "tker_comment_id_{$comment_id}", true );
+	$user_reactions    = $user_reactions ?: array();
+	$comment_reactions = get_comment_meta( $comment_id, "tker_reactions", true );
+	$comment_reactions = $comment_reactions ?: array();
+
+	if ( in_array( $emoji, $user_reactions, true ) ) {
+		$user_reacted = true;
+	}
+
+	if ( $user_reacted ) {
+		// Update User Meta.
+		$index = array_search( $emoji, $user_reactions, true );
+		unset( $user_reactions[ $index ] );
+		update_user_meta( $user_id, "tker_comment_id_{$comment_id}", $user_reactions );
+
+		// Update Comment Meta.
+		$count = $comment_reactions[ $emoji ];
+		$comment_reactions[ $emoji ] = --$count;
+		if ( $count === 0 ) {
+			unset( $comment_reactions[ $emoji ] );
+		}
+		update_comment_meta( $comment_id, 'tker_reactions', $comment_reactions );
+		wp_send_json_success( false );
+	} else {
+		// Update User Meta.
+		$user_reactions[] = $emoji;
+		update_user_meta( $user_id, "tker_comment_id_{$comment_id}", $user_reactions );
+
+		// Update Comment Meta.
+		$count = 0;
+		if ( isset( $comment_reactions[ $emoji ] ) ) {
+			$count = $comment_reactions[ $emoji ];
+		}
+		$comment_reactions[ $emoji ] = ++$count;
+		update_comment_meta( $comment_id, 'tker_reactions', $comment_reactions );
+		wp_send_json_success( true );
+	}
+
+	wp_send_json_error( 'Something went wrong.' );
+} );
